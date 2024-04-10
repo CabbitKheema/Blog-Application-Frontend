2c4ea24f817f6a2fb4f998fa791a7682d24dd025
CabbitKheema <161156792+CabbitKheema@users.noreply.github.com>
2024-04-10 20:34:58 +0530

    Created application header
    - Created a common Header.jsx
    - Added Flowbite React plugin supported by tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   );
