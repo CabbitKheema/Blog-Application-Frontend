457f8386d35b767a5b83071be115d89d125de361
CabbitKheema <161156792+CabbitKheema@users.noreply.github.com>
2024-04-11 22:24:04 +0530

    Added signup page with functionality

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:5000",
         secure: false,
       },
     },
